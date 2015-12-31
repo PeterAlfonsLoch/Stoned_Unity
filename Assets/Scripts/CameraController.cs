31e83d5f0e68b02ff13ab21f1cd18d878f343c05
shieldgenerator7 <shieldgenerator7@yahoo.com>
2015-12-31 02:01:48 -0500

    Pinch Zoom now works
    Pinching in and out zooms the camera in and out.
    The camera is still limited as to how far in or out it can zoom.
    
    NOTE: There is currently a bug where the pinch gestures causes the
    character to teleport. It will be fixed in a future commit.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,8 +8,11 @@ public class CameraController : MonoBehaviour {
 	private Vector3 offset;
     private Camera cam;
 
-	// Use this for initialization
-	void Start () {
+    public float perspectiveZoomSpeed = 0.5f; 
+    public float orthoZoomSpeed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
 		offset = transform.position - player.transform.position;
         cam = GetComponent<Camera>();
 
@@ -17,21 +20,73 @@ public class CameraController : MonoBehaviour {
 
     void Update()
     {
-        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        float minZoom = 1f;
+        float maxZoom = player.GetComponent<PlayerController>().baseRange * 2;
+        //
+        //Mouse Scrolling Zoom
+        //
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            if (cam.orthographicSize < player.GetComponent<PlayerController>().baseRange*2)
+            if (cam.orthographicSize < maxZoom)
             {
                 cam.orthographicSize += 1f;
             }
         }
         else if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            if (cam.orthographicSize > 1)
+            if (cam.orthographicSize > minZoom)
             {
                 cam.orthographicSize -= 1f;
             }
         }
 
+        //
+        //Pinch Touch Zoom
+        //2015-12-31 (1:23am): copied from https://unity3d.com/learn/tutorials/modules/beginner/platform-specific/pinch-zoom
+        //
+
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+            if (cam.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Make sure the orthographic size never drops below zero.
+                cam.orthographicSize = Mathf.Max(cam.orthographicSize, minZoom);
+                // Make sure the orthographic size never goes above maxZoom.
+                cam.orthographicSize = Mathf.Min(cam.orthographicSize, maxZoom);
+            }
+            else
+            {
+                // Otherwise change the field of view based on the change in distance between the touches.
+                cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 179.9f);
+            }
+        }
+
+        //
+        //Application closing
+        //
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();

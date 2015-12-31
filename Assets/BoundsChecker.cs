28a3ed4c4016b9983fdd333d826989c0b1c6bdd4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2015-12-31 01:01:25 -0500

    Added a bounds checker to keep things in the world
    So if anything falls off the map, it respawns at the center. This will
    probably be a temporary fix to decrease play tester annoyance. I don't
    think it will happen in the final game.

---
+++ Assets/BoundsChecker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundsChecker : MonoBehaviour {
+
+	//// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        coll.gameObject.transform.position = new Vector3(0, 0, coll.gameObject.transform.position.z);
+    }
+}

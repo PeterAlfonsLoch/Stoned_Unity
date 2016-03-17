2ff422a5d67387d8ce236e3532a62b8b82585dc9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-03-17 17:06:22 -0400

    Added milestones
    Added milestones
    -image
    -activator script
    
    NOTE: currently there is no special effect. That will come in a future
    update

---
+++ Assets/Scripts/MilestoneActivator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MilestoneActivator : MonoBehaviour {
+
+    private GameObject playerObject;
+    private bool used = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if ( ! used && coll.gameObject.Equals(playerObject))
+        {
+            used = true;
+            playerObject.GetComponent<PlayerController>().maxAirPorts++;
+            Destroy(this);//makes sure it can only be used once
+        }
+    }
+}
--- Assets/Scripts/SecretAreaTrigger.cs
+++ Assets/Scripts/SecretAreaTrigger.cs
@@ -10,15 +10,9 @@ public class SecretAreaTrigger : MonoBehaviour {
 	void Start () {
         playerObject = GameObject.FindGameObjectWithTag("Player");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        //FUTURE CODE: check to make sure the player is the one who collided before destroying the secret hider
         if (coll.gameObject.Equals(playerObject))
         {
             secretHider.AddComponent<Fader>();

2a848944ead6c809d3cbd7f65820c8cde17da0f0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-03-16 23:22:42 -0400

    Added secret areas
    And put some in the test level
    
    NOTE: Currently the part that hides it just disappears when you hit the
    trigger. In a future update, it will have some kind of effect.
    
    NOTE: Currently any game object can activate the trigger. In a future
    update, only the player can

---
+++ Assets/Scripts/SecretAreaTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretAreaTrigger : MonoBehaviour {
+
+    public GameObject secretHider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //FUTURE CODE: check to make sure the player is the one who collided before destroying the secret hider
+        GameObject.Destroy(secretHider);
+    }
+}

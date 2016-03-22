65143aa8a23a888a4a565c43bb735d73e9168b22
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-03-22 14:25:31 -0400

    Teleport Range Indicator disappears on cooldown
    Teleport Range Indicator disappears when Merky's teleport is on cool
    down

--- Assets/Scripts/TeleportRangeIndicatorUpdater.cs
+++ Assets/Scripts/TeleportRangeIndicatorUpdater.cs
@@ -8,29 +8,41 @@ public class TeleportRangeIndicatorUpdater : MonoBehaviour {
     private PlayerController controller;
     private float baseWidth=0;
     private float baseHeight=0;
+    SpriteRenderer sr;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         controller = parentObj.GetComponent<PlayerController>();
-            Vector3 size = GetComponent<SpriteRenderer>().bounds.size;
-            baseWidth = size.x;
-            baseHeight = size.y;
+        sr = GetComponent<SpriteRenderer>();
+        Vector3 size = sr.bounds.size;
+        baseWidth = size.x;
+        baseHeight = size.y;
     }
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //}
+    public void updateRange()
     {
-    }
-    public void updateRange() { 
+        if (controller.teleportTime <= Time.time)
+        {
+            sr.enabled = true;
+        }
+        else
+        {
+            sr.enabled = false;
+        }
         //transform.position = parentObj.transform.position;
         //transform.localScale = new Vector3(1, 1);
         float newSize = (float)controller.range * 2;
         //if (Mathf.Abs(baseWidth - newSize) > 1)
         //{
-            Vector3 newV = new Vector3(newSize / baseWidth, newSize / baseHeight);
-            //if (Mathf.Abs(transform.localScale.magnitude - newV.magnitude) > 1) {
-            transform.localScale = newV;
-       // }
+        Vector3 newV = new Vector3(newSize / baseWidth, newSize / baseHeight);
+        //if (Mathf.Abs(transform.localScale.magnitude - newV.magnitude) > 1) {
+        transform.localScale = newV;
+        // }
         //}
         //transform.localScale.y = newSize / height;
         //GetComponent<SpriteRenderer>().sprite.bounds.SetMinMax(controller.range * 2, controller.range * 2);

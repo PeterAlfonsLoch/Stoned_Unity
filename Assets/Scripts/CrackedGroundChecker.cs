2950c6fb84f54c184505fda5a10968aca0701664
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-04-06 23:11:58 -0400

    Added the cracked ground asset
    Added the cracked ground asset

---
+++ Assets/Scripts/CrackedGroundChecker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrackedGroundChecker : MonoBehaviour {
+
+    public float forceThreshold = 10;//how much force is required to break it
+    public AudioClip breakSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	//// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        GameObject other = coll.gameObject;
+        Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+        if (rb2d != null)
+        {
+            float force = rb2d.velocity.magnitude * rb2d.mass;
+            Debug.Log("force: " + force);
+            if (force > forceThreshold)
+            {
+                crackAndBreak();
+            }
+        }
+    }
+
+    private void crackAndBreak()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, transform.position);
+        Destroy(gameObject);
+    }
+}

aa7cf48decab1c69a1fecf6ce4648bc214652ab3
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-05-02 21:53:06 -0400

    Improved weight switch responsiveness
    Improved weight switch responsiveness by making it check its trigger
    area instead of responding to collisions (which bugs out in Unity when
    dealing with teleporting objects)

--- Assets/Scripts/WeightSwitchActivator.cs
+++ Assets/Scripts/WeightSwitchActivator.cs
@@ -4,36 +4,33 @@ using System.Collections;
 public class WeightSwitchActivator : MonoBehaviour {
 
     public bool pressed = false;
-    public GameObject weightObject;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        Vector3 otherPos = other.collider.gameObject.transform.position;
-        if (weightObject == null && otherPos.y > transform.position.y && Mathf.Abs(otherPos.x - transform.position.x) < 10)
+	void FixedUpdate () {
+        pressed = false;
+        //Determine if anything is in the weight switch's trigger area
+        BoxCollider2D bc2d = GetComponentInChildren<BoxCollider2D>();
+        if (bc2d != null)
         {
-            pressed = true;
-            weightObject = other.collider.gameObject;
-            //other.gameObject.transform.position = new Vector3(0, 0);
+            Collider2D[] hitColliders = Physics2D.OverlapAreaAll(bc2d.bounds.min, bc2d.bounds.max);
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                GameObject hc = hitColliders[i].gameObject;
+                if (!hc.Equals(gameObject) && !hc.Equals(bc2d.transform.gameObject))
+                {
+                    if (hc.GetComponent<Rigidbody2D>() != null)
+                    {
+                        Debug.Log("Weight switched: " + hc);
+                        pressed = true;
+                        break;
+                    }
+                }
+            }
         }
     }
-    void OnCollisionExit2D(Collision2D other)
-    {
-        //Vector3 otherPos = other.collider.gameObject.transform.position;
-        if (other.collider.gameObject.Equals(weightObject))
-        {
-            pressed = false;
-            weightObject = null;
-            //other.gameObject.transform.position = new Vector3(0, 0);
-        }
-    }
+    
 }

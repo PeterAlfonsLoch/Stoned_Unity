f174301af9111c50bb5a0373a141cd86f81ef44b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-08-16 21:40:20 -0400

    Made the hideable area work nicer with Ferr2D terrain
    Altho it still doesn't fade out the terrain like I want it to

--- Assets/Scripts/Fader.cs
+++ Assets/Scripts/Fader.cs
@@ -19,11 +19,17 @@ public class Fader : MonoBehaviour {
         duration = Mathf.Abs(startfade - endfade);
         srs = new ArrayList();
         srs.Add(GetComponent<SpriteRenderer>());
+        srs.Add(GetComponent<Ferr2DT_PathTerrain>());
         srs.AddRange(GetComponentsInChildren<SpriteRenderer>());
-        foreach (BoxCollider2D bc in GetComponentsInChildren<BoxCollider2D>())
+        srs.AddRange(GetComponentsInChildren<Ferr2DT_PathTerrain>());
+        foreach (Collider2D bc in GetComponentsInChildren<Collider2D>())
         {
             Destroy(bc);
             //bc.enabled = false;
+        }
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.sortingOrder = -1;
         }
     }
 
@@ -32,14 +38,29 @@ public class Fader : MonoBehaviour {
     {
         if (startTime <= Time.time)
         {
-            foreach (SpriteRenderer sr in srs)
+            foreach (Object o in srs)
             {
-                Color prevColor = sr.color;
-                float t = (Time.time - startTime) / duration;//2016-03-17: copied from an answer by treasgu (http://answers.unity3d.com/questions/654836/unity2d-sprite-fade-in-and-out.html)
-                sr.color = new Color(prevColor.r, prevColor.g, prevColor.b, Mathf.SmoothStep(startfade, endfade, t));
-                if (sr.color.a == endfade)
+                if (o is SpriteRenderer)
                 {
-                    Destroy(gameObject);
+                    SpriteRenderer sr = (SpriteRenderer)o;
+                    Color prevColor = sr.color;
+                    float t = (Time.time - startTime) / duration;//2016-03-17: copied from an answer by treasgu (http://answers.unity3d.com/questions/654836/unity2d-sprite-fade-in-and-out.html)
+                    sr.color = new Color(prevColor.r, prevColor.g, prevColor.b, Mathf.SmoothStep(startfade, endfade, t));
+                    if (sr.color.a == endfade)
+                    {
+                        Destroy(gameObject);
+                    }
+                }
+                if (o is Ferr2DT_PathTerrain)
+                {
+                    Ferr2DT_PathTerrain sr = (Ferr2DT_PathTerrain)o;
+                    Color prevColor = sr.vertexColor;
+                    float t = (Time.time - startTime) / duration;//2016-03-17: copied from an answer by treasgu (http://answers.unity3d.com/questions/654836/unity2d-sprite-fade-in-and-out.html)
+                    sr.vertexColor = new Color(prevColor.r, prevColor.g, prevColor.b, Mathf.SmoothStep(startfade, endfade, t));
+                    if (sr.vertexColor.a == endfade)
+                    {
+                        Destroy(gameObject);
+                    }
                 }
             }
         }

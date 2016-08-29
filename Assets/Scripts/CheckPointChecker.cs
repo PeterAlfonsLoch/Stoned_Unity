f31e603f06141e13205044dc66c234f8edc8f539
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-08-29 14:34:19 -0400

    First version of Checkpoints is working
    First version of Checkpoints is working
    
    NOTE: The check point ghosts don't disappear when you leave a
    checkpoint, and bouncing betweens checkpoints doesn't always work either

---
+++ Assets/Scripts/CheckPointChecker.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointChecker : MonoBehaviour {
+
+    public bool activated = true;
+    private GameObject ghost;
+    public GameObject ghostPrefab;
+
+	// Use this for initialization
+	void Start () {
+        ghost = (GameObject)Instantiate(ghostPrefab);
+        ghost.GetComponent<CheckPointGhostChecker>().sourceCP = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //When a player touches this checkpoint, activate it
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        activated = true;
+    }
+
+    /**
+    * When the player is inside, show the other activated checkpoints
+    */
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        activated = true;
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Checkpoint_Root"))
+        {
+            if (!go.Equals(this.gameObject))
+            {
+                go.GetComponent<CheckPointChecker>().showRelativeTo(this.gameObject);
+            }
+        }
+    }
+    //void OnTriggerExit2D(Collider2D coll)
+    //{
+    //    activated = true;
+    //    clearPostTeleport(true);
+    //}
+
+    /**
+    * Displays this checkpoint relative to currentCheckpoint
+    */
+    public void showRelativeTo(GameObject currentCheckpoint)
+    {
+        if (activated)
+        {
+            ghost.GetComponent<CheckPointGhostChecker>().curCP = currentCheckpoint;
+            ghost.SetActive(true);
+            ghost.transform.position = currentCheckpoint.transform.position + new Vector3(2,1,0);
+        }
+
+    }
+    /**
+    * So now the player has teleported and the checkpoint ghosts need to go away
+    */
+    public void clearPostTeleport(bool first)
+    {
+        ghost.SetActive(false);
+        if (first)
+        {
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("Checkpoint_Root"))
+            {
+                go.GetComponent<CheckPointChecker>().clearPostTeleport(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CheckPointGhostChecker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointGhostChecker : MonoBehaviour {
+
+    public GameObject sourceCP;//the checkpoint that this teleports to
+    public GameObject curCP;//the checkpoint that the player is currently at
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.Equals(GameObject.FindGameObjectWithTag("Player")))
+        {
+            coll.gameObject.transform.position = sourceCP.transform.position;
+            curCP.GetComponent<CheckPointChecker>().clearPostTeleport(true);
+        }
+    }
+}

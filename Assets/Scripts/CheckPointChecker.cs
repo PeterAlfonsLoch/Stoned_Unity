c96fca7314eb400b1c92a03f72d9f36249b8d758
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-09-05 21:46:32 -0400

    Fixes #35 CP Ghosts stay on screen
    Added a check to OnTriggerExit2D(.) to make sure that no teleport
    trigger could be activated before clearing all the checkpoint ghosts.

--- Assets/Scripts/CheckPointChecker.cs
+++ Assets/Scripts/CheckPointChecker.cs
@@ -6,12 +6,14 @@ public class CheckPointChecker : MonoBehaviour {
     public bool activated = false;
     private GameObject ghost;
     public GameObject ghostPrefab;
+    private GameObject player;
 
 	// Use this for initialization
 	void Start () {
         ghost = (GameObject)Instantiate(ghostPrefab);
         ghost.GetComponent<CheckPointGhostChecker>().sourceCP = this.gameObject;
         ghost.SetActive(false);
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -40,11 +42,26 @@ public class CheckPointChecker : MonoBehaviour {
             }
         }
     }
-    //void OnTriggerExit2D(Collider2D coll)
-    //{
-    //    activated = true;
-    //    clearPostTeleport(true);
-    //}
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        bool foundCPGhost = false;//whether the player is colliding with a checkpoint ghost
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Checkpoint_Root"))
+        {
+            if (!go.Equals(this.gameObject))
+            {
+                foundCPGhost = go.GetComponent<CheckPointChecker>().checkGhostActivation();
+                if (foundCPGhost)
+                {
+                    break;
+                }
+            }
+        }
+        if (!foundCPGhost)
+        {
+            activated = true;
+            clearPostTeleport(true);
+        }
+    }
 
     /**
     * Displays this checkpoint relative to currentCheckpoint
@@ -57,6 +74,13 @@ public class CheckPointChecker : MonoBehaviour {
             ghost.transform.position = currentCheckpoint.transform.position + new Vector3(2,1,0);
         }
 
+    }
+    /**
+    * Checks to see if the player is colliding with this checkpoint's ghost
+    */
+    public bool checkGhostActivation()
+    {
+        return ghost.GetComponent<BoxCollider2D>().bounds.Intersects(player.GetComponent<PolygonCollider2D>().bounds);
     }
     /**
     * So now the player has teleported and the checkpoint ghosts need to go away

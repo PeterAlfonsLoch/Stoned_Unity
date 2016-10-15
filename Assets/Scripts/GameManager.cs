ecb7e949cf91848862a2968fa815ced4fa69a7e9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-10-14 22:49:57 -0400

    Saves a list of game states
    It can now save a list of game states

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour {
     public GameObject root;
     public bool save = false;
     public bool load = false;
+    private static List<GameState> gameStates = new List<GameState>();
 
     private static Rigidbody2D rb2d;
 
@@ -40,10 +41,13 @@ public class GameManager : MonoBehaviour {
         file.Close();
         //bf.Serialize(file, savedGames);
         //RSManager.Serialize<GameObject>(root);
-        ES2.Save(root.transform,"merky.txt?tag=transform");
-        //ES2.Save(root.transform, "merky.txt?tag=transform2");
-        ES2.Save(rb2d.velocity, "merky.txt?tag=rb2dvelocity");
-        ES2.Save(rb2d.angularVelocity, "merky.txt?tag=rb2dangularvelocity");
+        GameState gs = new GameState(root.transform, rb2d.velocity, rb2d.angularVelocity);
+        gameStates.Add(gs);
+        ES2.Save(gameStates, "merky.txt");
+        //ES2.Save(root.transform,"merky.txt?tag=transform");
+        ////ES2.Save(root.transform, "merky.txt?tag=transform2");
+        //ES2.Save(rb2d.velocity, "merky.txt?tag=rb2dvelocity");
+        //ES2.Save(rb2d.angularVelocity, "merky.txt?tag=rb2dangularvelocity");
 
     }
     public static void Load(GameObject root)
@@ -54,10 +58,31 @@ public class GameManager : MonoBehaviour {
             //FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
             //savedGames = (List<GameObject>)bf.Deserialize(file);
             //file.Close();
-            ES2.Load<Transform>("merky.txt?tag=transform", root.transform);
-            //ES2.Load<Transform>("merky.txt?tag=transform2", root.transform);
-            rb2d.velocity = ES2.Load<Vector2>("merky.txt?tag=rb2dvelocity");
-            rb2d.angularVelocity = ES2.Load<float>("merky.txt?tag=rb2dangularvelocity");
+            //ES2.Load<Transform>("merky.txt?tag=transform", root.transform);
+            ////ES2.Load<Transform>("merky.txt?tag=transform2", root.transform);
+            //rb2d.velocity = ES2.Load<Vector2>("merky.txt?tag=rb2dvelocity");
+            //rb2d.angularVelocity = ES2.Load<float>("merky.txt?tag=rb2dangularvelocity");
+            GameState gs = ES2.Load<GameState>("merky.txt");
+            root.transform.position = gs.transform.position;
+            root.transform.localScale = gs.transform.localScale;
+            root.transform.rotation = gs.transform.rotation;
+            rb2d.velocity = gs.velocity;
+            rb2d.angularVelocity = gs.angularVelocity;
+
         }
+    }
+}
+
+public class GameState
+{
+    public Transform transform;
+    public Vector2 velocity;
+    public float angularVelocity;
+    public GameState() {}
+    public GameState(Transform t, Vector2 v, float av)
+    {
+        transform = t;
+        velocity = v;
+        angularVelocity = av;
     }
 }

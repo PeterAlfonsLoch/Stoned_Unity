29a44409d6068457a00d4978bde797cd95c83afd
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-10-14 21:56:53 -0400

    Now saves Merky's velocity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,8 +11,11 @@ public class GameManager : MonoBehaviour {
     public bool save = false;
     public bool load = false;
 
+    private static Rigidbody2D rb2d;
+
     // Use this for initialization
     void Start () {
+        rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -37,8 +40,11 @@ public class GameManager : MonoBehaviour {
         file.Close();
         //bf.Serialize(file, savedGames);
         //RSManager.Serialize<GameObject>(root);
-        ES2.Save(root.transform,"merky.txt");
-        
+        ES2.Save(root.transform,"merky.txt?tag=transform");
+        //ES2.Save(root.transform, "merky.txt?tag=transform2");
+        ES2.Save(rb2d.velocity, "merky.txt?tag=rb2dvelocity");
+        ES2.Save(rb2d.angularVelocity, "merky.txt?tag=rb2dangularvelocity");
+
     }
     public static void Load(GameObject root)
     {
@@ -48,7 +54,10 @@ public class GameManager : MonoBehaviour {
             //FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
             //savedGames = (List<GameObject>)bf.Deserialize(file);
             //file.Close();
-            ES2.Load<Transform>("merky.txt",root.transform);
+            ES2.Load<Transform>("merky.txt?tag=transform", root.transform);
+            //ES2.Load<Transform>("merky.txt?tag=transform2", root.transform);
+            rb2d.velocity = ES2.Load<Vector2>("merky.txt?tag=rb2dvelocity");
+            rb2d.angularVelocity = ES2.Load<float>("merky.txt?tag=rb2dangularvelocity");
         }
     }
 }

363ee8d8c79df9fe90a2fb954cd7129452e62736
shieldgenerator7 <shieldgenerator7@yahoo.com>
2016-10-14 21:15:31 -0400

    Now able to save/load Merky's position

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,8 @@ using System.IO;
 public class GameManager : MonoBehaviour {
     public static List<GameObject> savedGames = new List<GameObject>();
     public GameObject root;
-    public bool save = false;   
+    public bool save = false;
+    public bool load = false;
 
     // Use this for initialization
     void Start () {
@@ -21,25 +22,33 @@ public class GameManager : MonoBehaviour {
             save = false;
             Save(root);
         }
-	}
+        if (load == true)
+        {
+            load = false;
+            Load(root);
+        }
+    }
 
     public static void Save(GameObject root)
     {
         savedGames.Add(root);
         //BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
-        //bf.Serialize(file, savedGames);
-        RSManager.Serialize<GameObject>(root);
         file.Close();
+        //bf.Serialize(file, savedGames);
+        //RSManager.Serialize<GameObject>(root);
+        ES2.Save(root.transform,"merky.txt");
+        
     }
-    public static void Load()
+    public static void Load(GameObject root)
     {
         if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
-            savedGames = (List<GameObject>)bf.Deserialize(file);
-            file.Close();
+            //BinaryFormatter bf = new BinaryFormatter();
+            //FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            //savedGames = (List<GameObject>)bf.Deserialize(file);
+            //file.Close();
+            ES2.Load<Transform>("merky.txt",root.transform);
         }
     }
 }

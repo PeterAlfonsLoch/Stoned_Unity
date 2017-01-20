2b97e64dd5d8e78119d2519d19500050b7f3cfef
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-01-19 23:07:23 -0500

    Fixed #93 Shield Bubble Milestone
    There is now a milestone that grants the Shield Bubble ability.
    It is located in the GravityTestZone level.

---
+++ Assets/Scripts/MilestoneActivatorShieldBubble.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MilestoneActivatorShieldBubble : MilestoneActivator
+{//2017-01-19: copied from MilestoneActivatorForceTeleport
+
+    public override void activateEffect()
+    {
+        GameManager.getPlayerObject().GetComponent<ShieldBubbleAbility>().enabled = true;
+    }
+}

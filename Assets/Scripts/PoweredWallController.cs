58f2bc0eca8feb5085642858ab117d881151b827
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-01-24 22:42:33 -0500

    Fixed #105 Added Powered Door
    It goes up into the air when its power cube gets powered by a Shield
    Bubble

---
+++ Assets/Scripts/PoweredWallController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweredWallController : MonoBehaviour {
+    //2017-01-24: several things copied from FloatCubeController
+
+    public GameObject powerObj;
+    public float efficiency = 100;//how much force one unit of energy can generate
+    public float maxEnergyPerSecond;//the maximum amount of energy it can use per second
+    public float currentEnergy;
+
+    private Rigidbody2D rb;
+    private Vector3 upDirection;//used to determine the up direction of the powered door
+
+    // Use this for initialization
+    void Start ()
+    {
+        upDirection = transform.up;
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	//void Update () {		
+	//}
+
+    void FixedUpdate()
+    {
+        float amountGiven = powerObj.GetComponent<PowerCubeController>().giveEnergyToObject(maxEnergyPerSecond, Time.fixedDeltaTime);
+        currentEnergy += amountGiven;
+        if (currentEnergy > 0)
+        {
+            float energyToUse = Mathf.Min(currentEnergy, maxEnergyPerSecond) * Time.fixedDeltaTime;
+            if (energyToUse > 0)
+            {
+                currentEnergy -= energyToUse;
+                Vector3 forceVector = energyToUse * efficiency * 9.81f * upDirection;
+                Debug.DrawLine(transform.position, transform.position + forceVector, Color.green);
+                rb.AddForce(forceVector);
+            }
+        }
+        if (currentEnergy < 0)
+        {
+            currentEnergy = 0;
+        }
+    }
+}

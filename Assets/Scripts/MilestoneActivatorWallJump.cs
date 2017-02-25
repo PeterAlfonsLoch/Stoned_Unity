2ffdf037d5459be8a4984ab29a0cb0419baebe97
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-02-25 16:13:17 -0500

    Added Wall Jump Milestone
    Its ability also gets saved out and read back in

---
+++ Assets/Scripts/MilestoneActivatorWallJump.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilestoneActivatorWallJump : MilestoneActivator
+{//2017-02-25: copied from MilestoneActivatorAirport
+
+    public override void activateEffect()
+    {
+        GameManager.getPlayerObject().GetComponent<PlayerController>().wallJump = true;
+    }
+}

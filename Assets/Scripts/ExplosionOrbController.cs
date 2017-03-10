4805bd4f70e4d46127339508c16d9a10c5e617ec
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-09 21:22:45 -0500

    Got the explosion orb working
    It even has particle effects. It doesn't have tutorial options yet

--- Assets/Scripts/ExplosionOrbController.cs
+++ Assets/Scripts/ExplosionOrbController.cs
@@ -3,58 +3,33 @@ using System.Collections;
 
 public class ExplosionOrbController : MonoBehaviour {
     public int forceAmount = 300;
-    //public int pushTargets = 0;
-    ArrayList pushTargets;
+    private CircleCollider2D cc2D;
+    private ForceTeleportAbility fta;
+    private RaycastHit2D[] rh2ds = new RaycastHit2D[1];//used in Update() for a method call. Not actually updated
 
-	// Use this for initialization
-	void Start () {
-        pushTargets = new ArrayList();
-	
-	}
+    private float timeSinceLastExplosion = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        cc2D = GetComponent<CircleCollider2D>();
+        fta = GetComponent<ForceTeleportAbility>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-     void OnTriggerEnter2D(Collider2D other)
-    {
-        //Vector3 otherPos = other.collider.gameObject.transform.position;// = new Vector3(0,0);
-        //other.collider.gameObject.GetComponent<Rigidbody2D>().AddForce(otherPos - transform.position  * 500);
-        if (  ! pushTargets.Contains(other.gameObject))
+        if (cc2D.Cast(Vector2.zero, rh2ds, 0, true) > 0)
         {
-            pushTargets.Add(other.gameObject);
+            Debug.Log("Collision!");
+            if (Time.time - timeSinceLastExplosion > fta.maxHoldTime)
+            {
+                fta.processHoldGesture(transform.position, Time.time - timeSinceLastExplosion, true);
+                timeSinceLastExplosion = Time.time;
+            }
         }
-        foreach (GameObject go in pushTargets)
+        else
         {
-            //GameObject go = ((GameObject)obj);
-            //go.GetComponent<Rigidbody2D>().AddForce(go.transform.position - transform.position * -forceAmount);
-            
-            addExplosionForce(go.GetComponent<Rigidbody2D>(), forceAmount, transform.position, 10f);
+            fta.processHoldGesture(transform.position, Time.time - timeSinceLastExplosion, false);
         }
-        pushTargets.Clear();
-    }
-
-    public void addPushTarget(Object target)
-    {
-        pushTargets.Add(target);
-    }
-
-    public void removePushTarget(Object target)
-    {
-        pushTargets.Remove(target);
-    }
-
-    /**
-    * Retrieved on 2015-12-20 from http://forum.unity3d.com/threads/need-rigidbody2d-addexplosionforce.212173/
-    * Originally posted by Swamy
-    */
-    public static void addExplosionForce(Rigidbody2D body, float explosionForce, Vector3 explosionPosition, float explosionRadius)
-    {
-        Vector3 dir = (body.transform.position - explosionPosition);
-        float wearoff = 1 - (dir.magnitude / explosionRadius);
-        body.transform.position = Vector3.MoveTowards(body.transform.position, dir.normalized*explosionRadius + body.transform.position, 100 * Time.deltaTime);// * explosionRadius);
-        body.AddForce(dir.normalized * explosionForce * wearoff);
     }
 
 }

40f6b59b50bffb232654eddcf00fa11957ffe3ca
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-09 22:27:57 -0500

    Added toggles for force wave tutorial
    Explosion orb now has toggles for teaching the force wave ability. The
    toggles aren't set up optimally yet
    The force wave level beginning has also been adjusted to fit this new
    change

--- Assets/Scripts/ExplosionOrbController.cs
+++ Assets/Scripts/ExplosionOrbController.cs
@@ -6,29 +6,39 @@ public class ExplosionOrbController : MonoBehaviour {
     private CircleCollider2D cc2D;
     private ForceTeleportAbility fta;
     private RaycastHit2D[] rh2ds = new RaycastHit2D[1];//used in Update() for a method call. Not actually updated
+    
+    private float chargeTime = 0.0f;//amount of time spent charging
 
-    private float timeSinceLastExplosion = 0.0f;
+    //Tutorial toggles
+    public bool explodesUponContact = true;//false = requires mouseup (tap up) to explode
+    public bool chargesAutomatically = true;//false = requires hold to charge
+    public bool explodesAtAll = true;//false = doesn't do anything
 
     // Use this for initialization
     void Start () {
         cc2D = GetComponent<CircleCollider2D>();
         fta = GetComponent<ForceTeleportAbility>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (cc2D.Cast(Vector2.zero, rh2ds, 0, true) > 0 && !rh2ds[0].collider.isTrigger)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (explodesAtAll)
         {
-            Debug.Log("Collision!");
-            if (Time.time - timeSinceLastExplosion > fta.maxHoldTime)
+            if (explodesUponContact && cc2D.Cast(Vector2.zero, rh2ds, 0, true) > 0 && !rh2ds[0].collider.isTrigger)
             {
-                fta.processHoldGesture(transform.position, Time.time - timeSinceLastExplosion, true);
-                timeSinceLastExplosion = Time.time;
+                Debug.Log("Collision!");
+                if (chargeTime >= fta.maxHoldTime)
+                {
+                    fta.processHoldGesture(transform.position, chargeTime, true);
+                    chargeTime = 0;
+                }
             }
-        }
-        else
-        {
-            fta.processHoldGesture(transform.position, Time.time - timeSinceLastExplosion, false);
+            if (chargesAutomatically)
+            {
+                chargeTime += Time.deltaTime;
+                fta.processHoldGesture(transform.position, chargeTime, false);
+            }
         }
     }
 

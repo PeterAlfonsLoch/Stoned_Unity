37505da573db4fc8e54bff3e13d291fdfd45b5ce
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-12 16:04:56 -0400

    Added Editor Camera Rotator
    Now the camera rotates to match the rotation of the level you're looking
    at.

---
+++ Assets/Editor/EditorCameraRotator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(EditorCameraRotatorObject))]
+public class EditorCameraRotator : Editor
+{
+    EditorCameraRotatorObject ecro;
+
+    public void OnEnable()
+    {
+        ecro = (EditorCameraRotatorObject)target;
+        SceneView.onSceneGUIDelegate = rotateCamera;
+    }
+
+    void rotateCamera(SceneView sceneview)
+    {
+        if (ecro.autoRotate)
+        {
+            foreach (GravityZone gz in GameObject.FindObjectsOfType<GravityZone>())
+            {
+                if (gz.mainGravityZone)
+                {
+                    if (gz.GetComponent<PolygonCollider2D>().OverlapPoint(sceneview.camera.transform.position))
+                    {
+                        if (sceneview.camera.transform.localRotation != gz.gameObject.transform.localRotation)
+                        {
+                            sceneview.isRotationLocked = false;
+                            sceneview.camera.transform.localRotation = gz.gameObject.transform.localRotation;
+                            sceneview.camera.Render();
+                            ecro.rotZ = gz.transform.eulerAngles.z;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        else {
+            Quaternion angle = Quaternion.AngleAxis(ecro.rotZ, Vector3.forward);
+            if (sceneview.camera.transform.localRotation != angle)
+            {
+                sceneview.isRotationLocked = false;
+                sceneview.camera.transform.localRotation = angle;
+                sceneview.camera.Render();
+            }
+        }
+    }
+}

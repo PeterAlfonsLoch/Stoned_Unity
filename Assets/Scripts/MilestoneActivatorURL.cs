79e9247ecc3e2117c139074c9a45ebf7e76c4510
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-14 19:29:59 -0400

    Added the Patreon rock

---
+++ Assets/Scripts/MilestoneActivatorURL.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MilestoneActivatorURL : MilestoneActivator
+{//2017-03-14: copied from MilestoneActivatorAirport
+
+    public string url;//the url to open when activated
+
+    public override void activateEffect()
+    {
+        Application.OpenURL(url);
+    }
+}

1d77ff682da369eb1854ef930910a3f6554986bf
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-15 22:29:14 -0400

    Fixed #174 CP activate radius increased
    Going near a check point is enough to activate it now, it no longer
    requires Merky to touch it

---
+++ Assets/Scripts/CheckPointActivator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CheckPointActivator : MonoBehaviour
+{
+    //Place this on an object with a Collider
+    // w/ isTrigger checked
+    //and whose parent is a Checkpoint_Root
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            transform.parent.GetComponent<CheckPointChecker>().activate();
+        }
+    }
+}

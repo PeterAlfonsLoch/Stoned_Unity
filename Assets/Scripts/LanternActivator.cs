b4adf6231e002933e6290e0482667a6a87c29b29
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-21 21:11:35 -0400

    Fixed #184 Laterns light up near Merky

---
+++ Assets/Scripts/LanternActivator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternActivator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            foreach (ParticleSystem ps in GetComponentsInChildren<ParticleSystem>())
+            {
+                ps.Play();
+            }
+            Destroy(this);//delete this script
+        }
+    }
+}

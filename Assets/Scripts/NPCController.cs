23d205077157ef9a2a426a2f4da59b77db42d6a8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-03-28 21:48:05 -0400

    Added preliminary speech ability to all NPCs
    They all say the same thing tho, the dialogue needs to be written and
    recorded

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+
+    public AudioClip greeting;
+    AudioSource source;
+
+    bool triggerEntered = false;
+    bool audioPlaying = false;
+    float audioPlayEndTime = 0;
+    bool greetingPlayed = false;
+
+    private GameObject playerObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        playerObject = GameManager.getPlayerObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.transform.position = transform.position;
+        float distance = Vector3.Distance(playerObject.transform.position, transform.position);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, playerObject.transform.position - transform.position, distance);
+        int thingsFound = hits.Length;
+        //Debug.Log("Number things found: " + thingsFound);
+        if (distance < 5 && thingsFound == 2)
+        {
+            if (!greetingPlayed)
+            {
+                if (!source.isPlaying)
+                {
+                    audioPlaying = true;
+                    source.Play();
+                    //AudioSource.PlayClipAtPoint(greeting, transform.position);
+                    audioPlayEndTime = Time.time + greeting.length;
+                    greetingPlayed = true;
+                }
+            }
+        }
+    }
+}

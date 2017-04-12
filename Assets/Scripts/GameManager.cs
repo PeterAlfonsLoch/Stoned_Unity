4458ce994fb905746d371b045277f85046f5c3f0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-04-11 23:03:33 -0400

    Fixed #131 "objects not loading"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -31,7 +31,7 @@ public class GameManager : MonoBehaviour
     private CameraController camCtr;
     private float actionTime = 0;//used to determine how often to rewind
     private const float rewindDelay = 0.05f;//how much to delay each rewind transition by
-    private string newlyLoadedScene = null;
+    private List<string> newlyLoadedScenes = new List<string>();
     private string unloadedScene = null;
 
     // Use this for initialization
@@ -115,11 +115,14 @@ public class GameManager : MonoBehaviour
         {
             sl.check();
         }
-        if (newlyLoadedScene != null)
+        if (newlyLoadedScenes.Count > 0)
         {
             refreshGameObjects();
-            LoadObjectsFromScene(SceneManager.GetSceneByName(newlyLoadedScene));
-            newlyLoadedScene = null;
+            foreach (string s in newlyLoadedScenes)
+            {
+                LoadObjectsFromScene(SceneManager.GetSceneByName(s));
+            }
+            newlyLoadedScenes.Clear();
         }
         if (unloadedScene != null)
         {
@@ -135,7 +138,7 @@ public class GameManager : MonoBehaviour
     void sceneLoaded(Scene s, LoadSceneMode m)
     {
         refreshGameObjects();
-        newlyLoadedScene = s.name;
+        newlyLoadedScenes.Add(s.name);
     }
     void sceneUnloaded(Scene s)
     {

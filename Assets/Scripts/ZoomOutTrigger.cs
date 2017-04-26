b5c273971bc97aecf1fc5748ebcebe85729f7f62
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-04-25 20:47:35 -0400

    Fixed #199 Auto-zoom out at start

---
+++ Assets/Scripts/ZoomOutTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOutTrigger : MemoryMonoBehaviour {
+
+    public bool triggered = false;//whether or this has zoomed out the camera
+    	
+	void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            var camCtr = Camera.main.GetComponent<CameraController>();
+            camCtr.setScalePoint(camCtr.getScalePointIndex() + 1);//zoom out
+            GameManager.saveMemory(this);
+            Destroy(gameObject);
+        }
+    }
+    
+
+    public override MemoryObject getMemoryObject()
+    {
+        return new MemoryObject(this, triggered);
+    }
+    public override void acceptMemoryObject(MemoryObject memObj)
+    {
+        if (memObj.found)
+        {
+            triggered = true;
+            GameManager.saveMemory(this);
+            Destroy(gameObject);
+        }
+    }
+}

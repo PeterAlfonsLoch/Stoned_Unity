9ed66ce87a9c9563d534a55da77e5e005b8dc04e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-05-16 21:10:34 -0400

    MusicZones fade in and out

--- Assets/MusicZone.cs
+++ Assets/MusicZone.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class MusicZone : MonoBehaviour {
 
+    public float fadeTime = 2.0f;//how long it should take to fade in or out
+    private float fadeSpeed;//how fast it fades in or out
+
     private AudioSource music;
 
 	// Use this for initialization
 	void Start () {
         music = GetComponent<AudioSource>();
+        music.volume = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (music.isPlaying)
+        {
+            if (music.volume > 0 && fadeSpeed < 0
+                || music.volume < 1 && fadeSpeed > 0)
+            {
+                music.volume += fadeSpeed * Time.deltaTime;
+            }
+            if (music.volume <= 0)
+            {
+                music.Stop();
+                fadeSpeed = 0;
+            }
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
+            fadeSpeed = calculateFadeSpeed();
             music.Play();
         }
     }
@@ -27,7 +45,12 @@ public class MusicZone : MonoBehaviour {
     {
         if (coll.gameObject.tag == "Player")
         {
-            music.Stop();
+            fadeSpeed = -calculateFadeSpeed();
         }
+    }
+
+    float calculateFadeSpeed()
+    {
+        return 1.0f / fadeTime;
     }
 }

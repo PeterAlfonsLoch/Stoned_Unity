251ec67b995014e0b4456d0ff5ab3ef71b727bc5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-05-16 22:10:01 -0400

    Added MusicManager on Camera

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages which bgm to play.
+/// It takes "song requests" from multiple sources and decides which one to play
+/// </summary>
+public class MusicManager : MonoBehaviour {
+
+    private AudioSource currentSong;//the current song that is playing
+    private AudioSource prevSong;//the previous song that was playing
+
+    public float fadeTime = 2.0f;//how long it should take to fade in or out
+    private float fadeSpeed = 0;//how fast it fades in or out (determined by fadeTime)
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fadeSpeed != 0)
+        {
+            float shift = fadeSpeed * Time.deltaTime;
+            currentSong.volume += shift;
+            if (prevSong)
+            {
+                prevSong.volume -= shift;
+                if (prevSong.volume <= 0)
+                {
+                    prevSong.Stop();
+                    fadeSpeed = 0;
+                }
+            }
+            else if (currentSong.volume >= 1)
+            {
+                fadeSpeed = 0;
+            }
+        }
+	}
+
+    public void setCurrentSong(AudioSource newSong)
+    {
+        if (newSong != currentSong)
+        {
+            prevSong = currentSong;
+            currentSong = newSong;
+            currentSong.volume = 0;
+            currentSong.Play();
+            fadeSpeed = 1.0f / fadeTime;
+        }
+    }
+}
--- Assets/MusicZone.cs
+++ Assets/MusicZone.cs
@@ -4,53 +4,21 @@ using UnityEngine;
 
 public class MusicZone : MonoBehaviour {
 
-    public float fadeTime = 2.0f;//how long it should take to fade in or out
-    private float fadeSpeed;//how fast it fades in or out
-
     private AudioSource music;
+    private MusicManager musicManager;
 
 	// Use this for initialization
 	void Start () {
         music = GetComponent<AudioSource>();
         music.volume = 0;
+        musicManager = FindObjectOfType<MusicManager>();
 	}
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (music.isPlaying)
-        {
-            if (music.volume > 0 && fadeSpeed < 0
-                || music.volume < 1 && fadeSpeed > 0)
-            {
-                music.volume += fadeSpeed * Time.deltaTime;
-            }
-            if (music.volume <= 0)
-            {
-                music.Stop();
-                fadeSpeed = 0;
-            }
-        }
-    }
-
+    
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
-            fadeSpeed = calculateFadeSpeed();
-            music.Play();
+            musicManager.setCurrentSong(music);
         }
-    }
-    void OnTriggerExit2D(Collider2D coll)
-    {
-        if (coll.gameObject.tag == "Player")
-        {
-            fadeSpeed = -calculateFadeSpeed();
-        }
-    }
-
-    float calculateFadeSpeed()
-    {
-        return 1.0f / fadeTime;
     }
 }

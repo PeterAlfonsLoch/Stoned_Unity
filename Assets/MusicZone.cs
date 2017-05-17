cf4e8da0bcd4186958da4d0850af9ce3932ae41d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-05-16 20:41:08 -0400

    Added MusicZone and custom BGM

---
+++ Assets/MusicZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicZone : MonoBehaviour {
+
+    private AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+        music = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            music.Play();
+        }
+    }
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            music.Stop();
+        }
+    }
+}
